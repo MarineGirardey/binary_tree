a7b6c42e7745747e85b4f9be9a18c5e5d60730a5
Marine Girardey <marineland24@gmail.com>
2021-02-02 22:25:59 +0100

    binary_tree

---
+++ binary_tree.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 14:56:23 2021
+
+@author: Marine Girardey
+"""
+class Tree:
+    def __init__(self, rootNode):
+        self.rootNode = None
+
+    def traversal_deep(self):
+        self.
+
